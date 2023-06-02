d80e17af9be7825e2e45e03403626f219da41984
Tembora <134941928+Tembora@users.noreply.github.com>
2023-06-02 23:50:57 +0300

    Add files via upload

---
+++ задачи/less3task2.py
@@ -0,0 +1,12 @@
+#  Человек умелый - Homo habilis => Человек прямоходящий - Homo erectus =>
+#  Человек работающий - Homo ergaster => Гейдельбергский человек - Homo heidelbergensis =>
+#  Неандерталец - Homo neanderthalensis => Человек разумный	- Homo sapiens sapiens
+
+print("Введите 6 основных этапа развития человека:")
+s1 = input("Первый этап:")
+s2 = input("Второй этап:")
+s3 = input("Третий этап:")
+s4 = input("Четвертый этап:")
+s5 = input("Пятый этап:")
+s6 = input("Шестой этап:")
+print(s1, s2, s3, s4, s5, s6, sep="=>")
